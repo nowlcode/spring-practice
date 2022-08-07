6b47d6719267ad0e0e014866dff37630232c1e6a
nowlcode <yc4@wellesley.edu>
2022-08-07 23:59:33 +0900

    new changes

--- src/main/java/com/example/practice3/controller/MemberController.java
+++ src/main/java/com/example/practice3/controller/MemberController.java
@@ -1,4 +1,17 @@
 package com.example.practice3.controller;
 
+import com.example.practice3.dto.MemberDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
 public class MemberController {
+    @PostMapping
+    public void createMember(@RequestBody MemberDto memberDto){
+
+    }
+
 }
